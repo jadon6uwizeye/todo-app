be019d9326a2dc4ad9e2e3306f9fd415f697038d
UWIZEYE Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2022-06-21 17:49:50 +0200

    add stetfull widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,16 @@ class TodoApp extends StatefulWidget {
 }
 
 class _TodoAppState extends State<TodoApp> {
+  // ignore: prefer_final_fields
+  List<String> todoItems = <String>[];
+
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Todo List',
-        home: Scaffold(
+      title: 'Todo List',
+      home: Scaffold(
           backgroundColor: Colors.grey[900],
           appBar: AppBar(
             title: const Text('Todo List'),
@@ -22,16 +27,38 @@ class _TodoAppState extends State<TodoApp> {
             backgroundColor: Colors.grey[850],
             elevation: 0,
           ),
-          body: const Center(
-            child: Text(
-              'Todo List',
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.white,
-              ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                counter++;
+              });
+            },
+            tooltip: 'Add Task',
+            backgroundColor: Colors.grey[800],
+            child: const Icon(Icons.add),
           ),
-        )
-        ),
-        );
+          body: ListView.builder(
+            itemCount: counter,
+            itemBuilder: (BuildContext context, int index) {
+              if (index < todoItems.length) {
+                return ListTile(
+                  title: Text(
+                    todoItems[index],
+                    style: TextStyle(
+                      color: Colors.grey[50],
+                      fontSize: 20,
+                    ),
+                  ),
+                );
+              } else {
+                return const Text('No Items there',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 20,
+                    ));
+              }
+            },
+          )),
+    );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// ignore: unused_import
 import 'package:todo_app/main.dart';
 
 void main() {
